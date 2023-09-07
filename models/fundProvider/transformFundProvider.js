d4a59dd4937771da0bfd479751a23ae33af511b1
mahmoudsayed6767 <mahmoudsayed1006@gmail.com>
2023-09-07 17:41:29 +0300

    up

--- models/fundProvider/transformFundProvider.js
+++ models/fundProvider/transformFundProvider.js
@@ -30,11 +30,29 @@ export async function transformFundProvider(e, lang) {
     if(e.fundProviderOffer){
         let fundProviderOffer = {
             title:lang=="ar"?e.fundProviderOffer.title_ar:e.fundProviderOffer.title_en,
+            title_ar:e.fundProviderOffer.title_ar,
+            title_en:e.fundProviderOffer.title_en,
             offerType:e.fundProviderOffer.offerType,
             status:e.fundProviderOffer.status,
             monthlyPercent:e.fundProviderOffer.monthlyPercent,
+            startDate:e.fundProviderOffer.startDate,
+            endDate:e.fundProviderOffer.endDate,
             id:e.fundProviderOffer._id
         }
+        let programsPercent=[]
+        for (let val of e.fundProviderOffer.programsPercent) {
+            let monthlyPercent = {
+                monthlyPercent:val.monthlyPercent,
+            }
+            if(val.fundProgram){
+                monthlyPercent.fundProgram = {
+                    name:lang=="ar"?val.fundProgram.name_ar:val.fundProgram.name_en,
+                    id:val.fundProgram._id
+                }
+            }
+            programsPercent.push(monthlyPercent)
+        }
+        fundProviderOffer.programsPercent = programsPercent
         index.fundProviderOffer = fundProviderOffer
     }
     return index
